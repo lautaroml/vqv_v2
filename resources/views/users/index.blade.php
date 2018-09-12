b136e359292cd4858616cdadb92a024cc11bced6
Lautaro López <lautaroml@hotmail.com>
2018-09-12 18:06:47 -0500

    Added initial structure to users.index view. Send users from controller to view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,18 +4,31 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <div class="card">
-                    <div class="card-header">Dashboard</div>
-
+                    <div class="card-header">Usuarios</div>
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        You are logged in!
+                        <table cellpadding="1" cellspacing="1" class="table table-condensed table-hover" style="border-collapse: collapse;">
+                            <thead>
+                            <tr>
+                                <th style="text-align: center">Sub Estados</th>
+                                <th>Herramientas</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td style="text-align: center">
+                                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-xs btn-outline btn-success">
+                                            <i aria-hidden="true" class="fa fa-pencil-square-o fa-fw"></i>
+                                            Editar
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
