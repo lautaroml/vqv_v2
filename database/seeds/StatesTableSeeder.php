b18b87fa6f73169e7f675d2c2ee3e5fd68f185a1
Lautaro López <lautaroml@hotmail.com>
2018-09-13 16:18:26 -0500

    Added seeders for Countries and States

---
+++ database/seeds/StatesTableSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatesTableSeeder extends Seeder
+{
+
+    private $states = [
+        'Buenos Aires',
+        'Catamarca',
+        'Chaco',
+        'Chubut',
+        'Córdoba',
+        'Corrientes',
+        'Entre Ríos',
+        'Formosa',
+        'Jujuy',
+        'La Pampa',
+        'La Rioja',
+        'Mendoza',
+        'Misiones',
+        'Neuquén',
+        'Río Negro',
+        'Salta',
+        'San Juan',
+        'Santa Cruz',
+        'Santa Fe',
+        'Santiago del Estero',
+        'Tierra del Fuego',
+        'Tucumán'
+    ];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Truncate table
+        DB::table('states')->truncate();
+
+        // Populate table
+        $argentina = \App\Country::where('name', 'Argentina')->first();
+
+        foreach ($this->states as $state) {
+            DB::table('states')->insert([
+                'country_id' => $argentina->id,
+                'name' => $state
+            ]);
+        }
+    }
+}
